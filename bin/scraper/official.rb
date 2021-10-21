0e8d0c43940a6d6edf2926745f354f747b459a04
Tony Bowden <tony@tmtm.com>
2021-10-21 12:52:00 +0300

    Update to EPS 0.1.10

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,8 +5,7 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
+  class Member
     # Where the name in the header bar is different from the main list
     REMAP = {
       'Trygve Magnus Slagsvold Vedum (Centre Party)' => 'Trygve Slagsvold Vedum (Centre Party)',
@@ -40,23 +39,13 @@ class MemberList
     # From the header bar we can get a list of all minister+party names
     # (but only with their ministries, but not the actual position)
     def all_ministers
-      noko.xpath('//.').css('li a.dep-minister').map(&:text).map(&:tidy).map do |data|
+      @all_ministers ||= noko.xpath('//.').css('li a.dep-minister').map(&:text).map(&:tidy).map do |data|
         REMAP.fetch(data, data)
       end
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.flat_map do |member|
-        data = fragment(member => Member).to_h
-        [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
-      end
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('.listing .listItem')
     end
